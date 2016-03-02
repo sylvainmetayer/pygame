7f73f68fb92d345be774efadc40af8220ccff862
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-03-02 11:57:16 +0100

    Continue

--- serveur.py
+++ serveur.py
@@ -142,6 +142,9 @@ class ClientChannel(Channel, pygame.sprite.Sprite):
     def update_bar(self):
         self.bar.update()
 
+    def get_bar(self):
+        return self.bar
+
 
 class Ball(pygame.sprite.Sprite):
     """
@@ -151,7 +154,7 @@ class Ball(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png('images/balle.png')
         self.rect.center = outils.POS_BALLE
-        self.speed = [3, 3]
+        self.speed = [0, outils.BALL_SPEED]
         self.pas = 10
 
     def Network_ball(self, data):
@@ -159,12 +162,20 @@ class Ball(pygame.sprite.Sprite):
 
     def update(self):
          # On gère que la vitesse ne soit pas trop élevée.
-        if self.speed[0] >= 5 or self.speed[0] >= -5:
-            self.speed[0] = 3
-        if self.speed[1] >= 5 or self.speed[1] >= -5:
-            self.speed[1] = 3
+        #if self.speed[0] >= 5 or self.speed[0] >= -5:
+        #    self.speed[0] = 3
+        #if self.speed[1] >= 5 or self.speed[1] >= -5:
+        #    self.speed[1] = 3
 
         self.rect = self.rect.move(self.speed)
+
+    def reverse(self):
+        if self.speed[0] == 0:
+            self.speed[0] = outils.BALL_SPEED
+            self.speed[1] = 0
+        else:
+            self.speed[1] = outils.BALL_SPEED
+            self.speed[0] = 0
 
 class MyServer(Server):
     channelClass = ClientChannel
@@ -183,11 +194,11 @@ class MyServer(Server):
         self.clients.add(channel)
         if len(self.clients) == 1:
             print 'Je set la pos du J1'
-            clientTmp = self.clients.__getitem__(0)
+            clientTmp = self.clients.__getitem__(outils.J1)
             clientTmp.bar.set_position(outils.POS_J1)
         elif len(self.clients) == 2: # Joueur 2
             print "Je set la pos du J2"
-            clientTmp = self.clients.__getitem__(1)
+            clientTmp = self.clients.__getitem__(outils.J2)
             clientTmp.bar.set_position(outils.POS_J2)
             self.send_info("gameStart", "C'est parti !")
             print "Debut du jeu !"
@@ -232,6 +243,26 @@ class MyServer(Server):
         for client in self.clients:
             client.Send({"action":action, "message":message})
 
+    def get_all_clients(self):
+        liste = []
+        for client in self.clients:
+            liste.append(client.bar)
+        return liste
+
+    def collide_ball(self, balle, joueur):
+        print "Collide bar !!"
+        if joueur.rect.colliderect(balle.rect):
+            print "TOTO"
+            balle.reverse()
+            """if dx > 0: # Moving right; Hit the left side of the wall
+                self.rect.right = wall.rect.left
+            if dx < 0: # Moving left; Hit the right side of the wall
+                self.rect.left = wall.rect.right
+            if dy > 0: # Moving down; Hit the top side of the wall
+                self.rect.bottom = wall.rect.top
+            if dy < 0: # Moving up; Hit the bottom side of the wall
+                self.rect.top = wall.rect.bottom"""
+
     def launch_game(self):
         screen = self.screen
         background_image, background_rect = load_png('images/background.jpg')
@@ -252,6 +283,11 @@ class MyServer(Server):
                 self.update_balle()
                 self.send_bar()
                 self.send_balle()
+
+                # collisions joueur 1 avec la balle
+                self.collide_ball(self.balle, self.clients.__getitem__(outils.J1).get_bar())
+                self.collide_ball(self.balle, self.clients.__getitem__(outils.J2).get_bar())
+
                 screen.blit(background_image, background_rect)
             else:
                 screen.blit(background_load, background_load_rect)
